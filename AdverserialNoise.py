109b82d08481e9cbbf4c1da58e06d5eb4301b4c8
annaschroder <anna.schroder.18@ucl.ac.uk>
2025-08-11 18:21:36 +0100

    adding initial outline for adversarial script

---
+++ AdverserialNoise.py
@@ -0,0 +1,28 @@
+# import necessary libraries
+
+class AdversarialNoise:
+    '''A class to create adversarial examples'''
+    def __init__(self, model, input_data, target):
+        '''Initializes the AdversarialNoise class with a model, input data, and target.'''
+        self.model = model
+        self.input_data = input_data
+        self.target = target
+
+    def add_noise(self):
+        '''Adds adversarial noise to the input data.'''
+        return
+
+    def classify(self):
+        '''Classifies the input data.'''
+        
+        return
+
+    def loss_function(self):
+        '''Computes the loss between the model output and the target.'''
+        return
+
+    def opimise(self):
+        '''Optimizes the model parameters based on the computed loss.'''
+        return
+
+
